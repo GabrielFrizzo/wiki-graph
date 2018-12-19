1ed1fe2f0b75d0e19db4593efa2fca5dff9eb0db
Gabriel Frizzo <gabrielfrizzo@gmail.com>
2018-12-19 13:06:58 -0200

    Fix issues with traversal

--- wiki_graph.py
+++ wiki_graph.py
@@ -8,17 +8,28 @@ from re import match
 BASE_LINK = 'https://en.wikipedia.org'
 
 def valid_link(link):
-	if match('/wiki/[A-Za-z0-9\(\)_]*$', link) is not None:
-		return True
-	return False
+	try:
+		if match('/wiki/[A-Za-z0-9\(\)_]*$', link) is not None:
+			return True
+		return False
+	except:
+		print('Error finding wiki link in:')
+		print(link)
+		return False
 
 def find_links(current_page):
 	with request.urlopen(BASE_LINK + current_page) as response:
 		html = response.read()
 	soup = BeautifulSoup(html, 'html.parser')
 
+	links = []
 	for paragraph in soup.find(id='mw-content-text').find_all('p'):
-		return [link for link in paragraph.find_all('a') if valid_link(link)]
+		for link in paragraph.find_all('a'):
+			href = link.get('href')
+			if valid_link(href):
+				links.append(href)
+
+	return links
 
 def page_label(page):
 	return page[6:]
@@ -29,7 +40,7 @@ def add_page(depth, curr_page, prev_page = None):
 		G.add_edge(page_label(prev_page), page_label(curr_page))
 
 	if depth > 0:
-		for next_page in find_links(curr_page):
+		for next_page in find_links(curr_page)[:7]:
 			print(curr_page, next_page)
 			add_page(depth-1, next_page, curr_page) 
 
@@ -37,7 +48,7 @@ def add_page(depth, curr_page, prev_page = None):
 def populate_graph():
 	first_page = '/wiki/Freddie_Mercury'
 
-	add_page(1, first_page)
+	add_page(4, first_page)
 
 G = nx.DiGraph()
 
